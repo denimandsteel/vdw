9a4ec204d48afa8d0649d815d1d1df3ab5f07391
Tylor Sherman <tylorsherman@gmail.com>
2017-05-01 17:53:24 -0700

    Docs

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
+# To run this script:
+#   1. cd ~/Desktop/vdw/
+#   2. sh build.sh
+# Then just check things out in your Git client, commit, and sync!
+
 # cd events/ && ruby fetch_events.rb && git add _posts/ && git commit -m "Build events" && cd ../
 cd events/ && ruby fetch_events.rb && git add _posts/ && cd ../
